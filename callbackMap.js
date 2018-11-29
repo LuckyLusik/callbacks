a9295892ce071ceb05962851493a65958df9805b
Lucy <lucyneatdesign@yahoo.ca>
2018-11-29 19:43:31 +0000

    Immitation of map

---
+++ callbackMap.js
@@ -0,0 +1,26 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+function map(arr, func){
+  var result = [];
+  for (var arr1 of arr){
+    result.push(func(arr1));
+  }
+  console.log(result);
+}
+// [6, 7, 2, 5, 3]
+
+// [ "GROUND", "CONTROL", "TO", "MAJOR", "TOM" ]
+
+// [ 'dnuorg', 'lortnoc', 'ot', 'rojam', 'mot' ]
